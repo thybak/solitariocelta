7ab8ffd7eb75dda4f7d54256056c471231caa734
David M. Martín <davidm.martin@outlook.com>
2017-05-15 22:33:01 +0200

    Initial commit

---
+++ app/models/Usuario.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    //
+    /**
+     * Nombre de la tabla de usuarios
+     * @var string
+     */
+    protected $table = 'usuarios';
+}
